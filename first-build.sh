631df43b56c4f7aad79ac1349048c1e1db31fe23
Sanjay Govind <sanjay.govind9@gmail.com>
2018-11-19 21:19:01 +1300

    Mostly complete implementation of settings, circleci config

--- first-build.sh
+++ first-build.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
-cd scratch-vm
-npm install
-npm link
-#npm run watch
-cd ../scratch-gui
-npm install
-npm link scratch-vm
-npm start
+if [ $CIRCLE_BRANCH == $SOURCE_BRANCH ]; then
+    git config --global user.email $GH_EMAIL
+    git config --global user.name $GH_NAME
+
+    git clone $CIRCLE_REPOSITORY_URL out
+
+    cd out
+    git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+    git rm -rf .
+    cd ..
+    cd scratch-vm
+    npm install
+    npm link
+    #npm run watch
+    cd ../scratch-gui
+    npm install
+    npm link scratch-vm
+    npm build
+    cd ../
+    cp -a scratch-gui/build/. out/.
+
+    mkdir -p out/.circleci && cp -a .circleci/. out/.circleci/.
+    cd out
+
+    git add -A
+    git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}" --allow-empty
+
+    git push origin $TARGET_BRANCH
+fi
